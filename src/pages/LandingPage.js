2139a07719d0c705a267679c1644493aa4959a15
Jocerdikiawann <cocokcocok33@gmail.com>
2020-10-21 05:57:51 +0700

    second Comment

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import Header from 'parts/Header'
+
+export default function LandingPage() {
+    return (
+        <>
+        {/* es6 */}
+         <Header {...this.props}>
+
+         </Header>   
+        </>
+    )
+}
