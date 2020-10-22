2d310145dca7cc49c950d810ea3ae66049295863
Jocerdikiawann <cocokcocok33@gmail.com>
2020-10-22 16:13:07 +0700

    Finished Mostpick and categories

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,28 @@
-import React, {Component} from 'react'
-import Header from 'parts/Header'
+import React, { Component } from "react";
+import Header from "parts/Header";
+import Hero from "parts/Hero";
+import landingPage from "json/landingPage.json";
+import MostPicked from "parts/MostPicked";
+import Categories from 'parts/Categories'
 
-export default function LandingPage() {
+export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
+  render() {
     return (
-        <>
-        {/* es6 */}
-         <Header {...this.props}>
+      <>
+        <Header {...this.props}></Header>
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
 
-         </Header>   
-        </>
-    )
+        <Categories data={landingPage.categories} />
+
+      </>
+    );
+  }
 }
---
+++ src/utils/formatNumber.js
@@ -0,0 +1,5 @@
+// api bawaan browser
+export default (number) => {
+  const formatNumbering = new Intl.NumberFormat("id-ID");
+  return formatNumbering.format(number);
+};
