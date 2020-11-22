9415943ef29a367df5a910613a60ef4bd39456f2
Jocerdikiawann <cocokcocok33@gmail.com>
2020-11-22 19:35:52 +0700

    Commit lagi

--- src/parts/Checkout/Completed.js
+++ src/parts/Checkout/Completed.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
-import Complit from "assets/images/completed.jpg";
+// import Complit from "assets/images/completed.jpg";
 
 export default function Completed() {
   return (
@@ -8,7 +8,7 @@ export default function Completed() {
       <div className="container" style={{ marginBottom: 30 }}>
         <div className="row justify-content-center text-center">
           <div className="col-4">
-            <img src={Complit} alt="Completed Image" className="img-fluid" />
+            {/* <img src={Complit} alt="Completed Image" className="img-fluid" /> */}
             <p className="text-gray-500">
               We will inform you via email later once the transaction has been
               accepted
